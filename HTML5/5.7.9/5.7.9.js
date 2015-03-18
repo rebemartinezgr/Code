ccf1423cefbb2fc585fb3b9fa424a88d3c8a3494
rebeca <marg@lambda08.aulas.gsyc.es>
2015-03-18 14:30:33 +0100

    ejercicio clase funcionalidad

---
+++ HTML5/5.7.9/5.7.9.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+if (Modernizr.geolocation) {
+	alert("acepta geolocation");
+} else {
+	alert("no acepta  geolocation");
+}
+}
+/*canvas, video, video.ogg, localstorage, webworkers, applicationcache,
+geolocation, input.autofocus, history*/
