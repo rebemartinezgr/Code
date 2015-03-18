db0f2f4e5cad50ebb33855254267bffbe9bf3c27
rebeca <marg@lambda08.aulas.gsyc.es>
2015-03-18 14:45:46 +0100

    ejercicios

--- jQuery/5.6.6.js
+++ jQuery/5.6.6.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
 
 $("#botton").click(function(){
-	$.getJSON("http://localhost:8000/5.6.7.json", function(data){
+	$.getJSON("http://localhost:8000/Code/jQuery/5.6.6.json", function(data){
 		$("#text").append(data.frase1);
 	
 	})
