df04b07affe75d8fb05fb7b10ad10846dbace678
rebemartinezgr <r.martinezgr@alumnos.urjc.es>
2015-03-23 18:09:11 +0100

    ejercicios clase

--- jQuery/5.6.6.js
+++ jQuery/5.6.6.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
 $("#botton").click(function(){
 	$.getJSON("http://localhost:8000/Code/jQuery/5.6.6.json", function(data){
-		$("#text").append(data.frase1);
+		$("#text").html(data.frase1);
 	
 	})
 });
